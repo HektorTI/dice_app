1261c8982406493600c9005a8e79093bd0733823
HektorTI <heitorazevdo21@gmail.com>
2023-09-26 12:22:56 -0400

    main class optmize

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,11 @@
+import 'package:dice_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 93, 41, 184),
-                Color.fromARGB(255, 53, 144, 136),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.centerRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world!',
-              style: TextStyle(
-                color: Colors.amberAccent,
-                fontSize: 28.0,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
