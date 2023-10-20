5477bad72231ab55234ece8872f1ec2a7e19b874
HektorTI <heitorazevdo21@gmail.com>
2023-10-20 07:30:48 -0400

    retirei Myapp na pasta teste

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GradientContainer(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 65, 64, 62),
+          title: const Text('Dice App'),
+        ),
+        body: const GradientContainer(
           Color.fromARGB(255, 183, 58, 62),
           Color.fromARGB(255, 73, 58, 183),
         ),
